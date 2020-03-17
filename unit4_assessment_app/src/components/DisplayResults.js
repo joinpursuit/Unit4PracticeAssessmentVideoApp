2f8762139a7f8facea2b9c316c9e9b92893ab79c
Kevin Wong <kevinwong@pursuit.org>
2020-03-16 23:15:00 -0400

    complete axios call and display thumbnails for videos

--- unit4_assessment_app/src/components/DisplayResults.js
+++ unit4_assessment_app/src/components/DisplayResults.js
@@ -1,15 +1,23 @@
 import React from "react";
 
 const DisplayResults = ({ videos }) => {
-  return (
-    <div className="displayResults">
-      {videos ? (
-        <p>No Search Results Yet! Please submit a search above!</p>
-      ) : (
-        <p>{videos}</p>
-      )}
-    </div>
-  );
+  //   const displayVideos = () => {
+  //     let showVideos = videos.map(video => {
+  //       return <image src={video.snippet.thumbnails.medium.url}></image>;
+  //     });
+  //     return showVideos;
+  //   };
+
+  let showVideos = videos.map(video => {
+    return (
+      <img
+        key={video.id.videoId}
+        src={video.snippet.thumbnails.medium.url}
+      ></img>
+    );
+  });
+
+  return <div className="displayResults">{showVideos}</div>;
 };
 
 export default DisplayResults;
--- unit4_assessment_app/src/components/HomePage.js
+++ unit4_assessment_app/src/components/HomePage.js
@@ -7,43 +7,37 @@ import API_KEY from "../secrets";
 import axios from "axios";
 
 const HomePage = () => {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState(null);
   const searchInputObj = useInput("");
-
-  let youtube = axios.create({
-    baseURL: `https://www.googleapis.com/youtube/v3/`
-    // params: {
-    //   part: "snippet",
-    //   maxResults: 8,
-    //   key: API_KEY
-    // }
-  });
 
   const handleSubmit = async e => {
     e.preventDefault();
-    debugger;
     try {
-      const res = await youtube.get(`/search`, {
-        params: {
-          part: "snippet",
-          maxResults: 8,
-          key: API_KEY,
-          q: searchInputObj.value
+      const res = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search`,
+        {
+          params: {
+            part: "snippet",
+            maxResults: 8,
+            key: API_KEY,
+            q: searchInputObj.value
+          }
         }
-      });
-      debugger;
-      setVideos([]);
+      );
+      setVideos(res.data.items);
     } catch (error) {
       console.log(error);
     }
   };
 
-  console.log(videos);
-  console.log(searchInputObj.value);
   return (
     <div className="home">
       <Search handleSubmit={handleSubmit} searchInputObj={searchInputObj} />
-      <DisplayResults videos={videos} />
+      {!videos ? (
+        <p>No Search Results Yet! Please submit a search above!</p>
+      ) : (
+        <DisplayResults videos={videos} />
+      )}
     </div>
   );
 };
