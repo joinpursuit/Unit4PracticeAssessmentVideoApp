e0836d4bc93309cce286641a7221e62c2881f168
BrandonBrown <brandonbrown@pursuit.org>
2020-03-16 21:55:21 -0400

    add navbar to header and add css

--- youtubeapp/src/Components/UniversalComponents/Body.js
+++ youtubeapp/src/Components/UniversalComponents/Body.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Body = () => {
 return(
     <div className="bodyDiv">
-
+        <h1>BODY</h1>
     </div>
 )
 }
--- youtubeapp/src/Components/UniversalComponents/Header.js
+++ youtubeapp/src/Components/UniversalComponents/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const Header = () => {
     return(
         <div className="header">
-
+            <h1>HEADER</h1>
+            <NavBar/>
         </div>
     )
 }
