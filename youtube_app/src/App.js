9815701893e7ec7789388033a350cfe1c116d6f4
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-03-16 20:50:31 -0400

    begin navbar in app.js

--- youtube_app/src/App.js
+++ youtube_app/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import About from '../frontend/src/components/About';
+import Home from '../frontend/src/components/Home';
+import Video from '../frontend/src/components/Video';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch >
+        <Route exact path={"/"}>
+            <About/>
+        </Route>
+        <Route path={"/signup"}>
+            <Home/>
+        </Route>
+        <Route path={"/homepage"}>
+            <Video/>
+        </Route>
+      </Switch>
     </div>
   );
 }
