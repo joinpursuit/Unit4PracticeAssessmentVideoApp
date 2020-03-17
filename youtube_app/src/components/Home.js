afe04f7ea89c52e72c549647f04cbc5de2bb26e2
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-03-16 22:03:39 -0400

    begin adding functions to Home.js component

--- youtube_app/src/components/Home.js
+++ youtube_app/src/components/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    const [search, setSearch] = useState("");
+
+    const fetchData= async(url, setData)=>{
+        let res = await axios.get(url)
+        try {
+            res.data.payload.map((el) => {
+                return setData(prevState => [...prevState, el.tag_name])
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const handleSearch = (e) => {
+        e.preventDefault() 
+        window.location="../home"
+        sessionStorage.searchTerm=e.target.elements[0].value
+    };
+
+    useEffect(()=>{
+        fetchData("https://www.googleapis.com/youtube/v3/search", setSearch)
+    }, [])
 
     return(
         <div className="homeDiv">
-            <form className="searchForm">
+            <form className="searchForm" onSubmit={handleSearch}>
                 <input type="search" placeholder="Search..."></input>
                 <button type="button">Search</button>
             </form>
