01d96a21edfc3979ec0828413ebd88c4d238300a
Nilber Remon <nilberremon@gmail.com>
2020-03-18 19:17:15 -0400

    Remove File

--- youtube-client-app/src/Components/api.js
+++
@@ -1,7 +0,0 @@
-https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&key=AIzaSyBj7-ChIdixunYA76SJAeo6qvUWwR--QKU
-
-https://www.googleapis.com/youtube/v3/search?part=snippet&type=playlist&maxResults=25&key=AIzaSyBj7-ChIdixunYA76SJAeo6qvUWwR--QKU
-
-https://www.googleapis.com/youtube/v3/videoCategories
-
-
