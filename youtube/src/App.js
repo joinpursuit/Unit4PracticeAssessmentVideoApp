7afdf7bda12293a24dbd7dbfe44daf3d1f48dfbb
DejaLFlynn <dejaflynn@pursuit.org>
2020-03-18 18:43:09 -0400

    started search component working on values

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -4,8 +4,8 @@ import NavBar from './Components/Navbar';
 import Home from './Components/Home';
 import HomePage from './Components/HomePage'
 import About from './Components/About';
-import {Route, Switch} from 'react-dom'
 import Search from './Components/Search';
+import {Route, Switch} from 'react-dom'
 
 function App() {
   return (
--- youtube/src/Components/Search.js
+++ youtube/src/Components/Search.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
 import { useInput } from '../Utilities/Input';
-
+import axios from 'axios'
+import API from '../secret'
 
 const Search = ()=>{
     const searchVideos = useInput("")
+    const [videos, setVideos]=useState([]);
+    const getVideos = async()=>{
+        const userVideos = (``)
+        try{
+            let res = await axios.get(userVideos)
+            setVideos(res.data)
+        }catch(error){
+            setVideos([])
+        }
+        }
+    
+        // useEffect(()=>{
+        //     getVideos()
+           
+        // }, [])
 
     return (
         <div className="Search">
