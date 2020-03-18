21d890717c649ccdd0cb7b0e622bea6503919869
bsihame <sihamebazi@pursuit.org>
2020-03-17 22:19:46 -0400

    add narbar components

---
+++ youtube-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+const Navbar = () => {
+  return(
+    <nav>
+      <div>Youtube</div>
+      <div>Home</div>
+      <div>About</div>
+    </nav>
+  )
+}
+export default Navbar
