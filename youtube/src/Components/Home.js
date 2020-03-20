7abac2df50031c3a46c37c83661d4bff6f669951
ashya <ashyamanning@pursuit.org>
2020-03-19 21:35:18 -0400

    handle api search

--- youtube/src/Components/Home.js
+++ youtube/src/Components/Home.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from 'react';
+import API_KEY from "../secrets";
+import axios from 'axios';
 
 class Home extends React.Component {
     state = { search: "" }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
+        try {
+            await axios.get(`https://www.googleapis.com/youtube/v3/search?maxResults=2&type=video&key=${API_KEY}&part=snippet&q=${this.state.search}`); 
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     handleSearchChange = (e) => {
         this.setState({search: e.target.value});
     }
 
-    handleApiSearch = async () => {
-        try {
-            
-        } catch (err) {
-            console.log(err)
-        }
-    }
 
     render() {
         console.log(this.state);
