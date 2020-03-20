a1c978a36370f669f55ce8fdd73a5a7c5c0789ea
maria liza <mariamartinez@pursuit.org>
2020-03-20 17:27:19 -0400

    add more styling

--- practice/src/components/Home/Home.js
+++ practice/src/components/Home/Home.js
@@ -1,21 +1,8 @@
 import React from "react";
+import "./../../css/Home.css";
 
 const Home = () => {
-  return <div>
-  Hello, I'm Home!
-  </div>;
+  return <div>Hello, I'm Home!</div>;
 };
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default Home;
