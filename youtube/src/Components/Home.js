34483dd586e53c02b123083c8d4bc66f034e09a2
ashya <ashyamanning@pursuit.org>
2020-03-19 23:20:43 -0400

    get video Info array

--- youtube/src/Components/Home.js
+++ youtube/src/Components/Home.js
@@ -1,43 +1,59 @@
-import React from 'react';
+import React from "react";
 import API_KEY from "../secrets";
-import axios from 'axios';
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
-    state = { search: "" }
+  state = { search: "", results: [] };
 
-    handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await axios.get(`https://www.googleapis.com/youtube/v3/search?maxResults=2&type=video&key=${API_KEY}&part=snippet&q=${this.state.search}`); 
-        } catch (err) {
-            console.log(err);
-        }
+  handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      let res = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search?maxResults=1&type=video&key=${API_KEY}&part=snippet&q=${this.state.search}`
+      );
+      // console.log(res.data.items);
+      this.setState({ results: res.data.items });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    handleSearchChange = (e) => {
-        this.setState({search: e.target.value});
-    }
+  handleSearchChange = e => {
+    this.setState({ search: e.target.value });
+  };
 
-
-    render() {
-        console.log(this.state);
-        let { search } = this.state;
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" 
-                    placeholder="Enter a Search"
-                    name="search"
-                    value={search}
-                    onChange={this.handleSearchChange}/>
-                    <button type="submit">Search</button>
-    
-                </form>
-            </div>
-    
-        )
-
-    }
+  render() {
+    console.log(this.state);
+    let { search, results } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit} required>
+          <input
+            type="text"
+            placeholder="Enter a Search"
+            name="search"
+            value={search}
+            onChange={this.handleSearchChange}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <div>
+          {results.map(item => {
+            return (
+              <Link to={`/video/${item.id.videoId}`} key={item.id.videoId}>
+                <p>{item.snippet.title}</p>
+                <img
+                  src={item.snippet.thumbnails.medium.url}
+                  alt={item.snippet.title}
+                />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
--- youtube/src/Components/Video.js
+++ youtube/src/Components/Video.js
@@ -1,49 +1,74 @@
-import React, {useState} from 'react';
-import { useParams } from 'react-router-dom';
-import YouTube from 'react-youtube';
-
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import YouTube from "react-youtube";
 
 const Video = () => {
-    const [name, setName] = useState();
-    const [comment, setComment] = useState();
-    const [comments, setComments] = useState([])
-    let { id } = useParams();
-    // 👆 populated by the route/url
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
+  const [videoInfo, setVideoInfo] = useState([]);
+  let { id } = useParams();
+  // 👆 populated by the route/url
 
-    const handleName = (e) => {
-        setName(e.target.value);
-    }
+  //   const handleName = e => {
+  //     setName(e.target.value);
+  //   };
 
-    const handleComment = (e) => {
-        setComment(e.target.value);
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // add name and comment to some comments array
-    
-    }
+  //   const handleComment = e => {
+  //     setComment(e.target.value);
+  //   };
 
-    return (
+  const handleSubmit = e => {
+    e.preventDefault();
+    // add name and comment to some comments array
+    let timeStamp = new Date().toLocaleString();
+    let commentInfo = { name, comment, timeStamp };
+    setVideoInfo([commentInfo, ...videoInfo]);
+    setName("");
+    setComment("");
+  };
+
+  // console.log(name, comment);
+  console.log(videoInfo);
+  return (
     <>
-    
-        <YouTube
-          videoId={id}
-        />
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type="text" name="name" value={name} onChange={handleName} />
-            </label>
-            <label>
-                Comment:
-                <input type="text" name="comment" value={comment} onChange={handleComment}/>
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-        {/* render the contents of the `comments` array */}
+      <YouTube videoId={id} />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Comment:
+          <input
+            type="text"
+            name="comment"
+            value={comment}
+            onChange={e => setComment(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {/* render the contents of the `comments` array */}
+      <div>
+        {videoInfo.map(comment => {
+          return (
+            <div key={comment.timeStamp}>
+              <p>{comment.name}</p>
+              <p>{comment.comment}</p>
+              <p>{comment.timeStamp}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
-    );
-}
+  );
+};
 
 export default Video;
