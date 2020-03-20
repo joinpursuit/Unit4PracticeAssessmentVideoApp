0b76ceb420c70cb42332ff13e8adbef4ce84d322
Kevin Wong <kevinwong@pursuit.org>
2020-03-20 13:00:28 -0400

    add Youtube component to VideoPage component

--- unit4_assessment_app/src/components/VideoPage.js
+++ unit4_assessment_app/src/components/VideoPage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Youtube from "react-youtube";
 
 const VideoPage = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1
+    }
+  };
+  const _onReady = e => {
+    // access to player in all event handlers via event.target
+    e.target.pauseVideo();
+  };
+  return (
+    <div>
+      <Youtube videoId={id} opts={opts} onReady={_onReady} />
+    </div>
+  );
 };
 
 export default VideoPage;
