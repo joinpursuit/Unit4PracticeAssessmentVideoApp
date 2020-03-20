2c723fca7c9237b06ece430aab7ac93263d485b7
Kevin Wong <kevinwong@pursuit.org>
2020-03-20 18:08:43 -0400

    add css for NavBar component

--- unit4_assessment_app/src/components/NavBar.js
+++ unit4_assessment_app/src/components/NavBar.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { NavLink, Link, useHistory } from "react-router-dom";
+import "../css/NavBar.css";
 
 const NavBar = () => {
   //   let history = useHistory();
   return (
-    <nav>
-      <NavLink to={"/youtube"}>
-        <h1>Youtube</h1>
-      </NavLink>
-      <NavLink exact to={"/"}>
-        Home
-      </NavLink>
-      <NavLink to={"/about"}>About</NavLink>
-    </nav>
+    <header>
+      <nav>
+        <NavLink to={"/youtube"}>
+          <img
+            id="youtubeIcon"
+            src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/youtube_square_color-512.png"
+            alt="Youtube"
+          />
+        </NavLink>
+        <NavLink exact to={"/"}>
+          Home
+        </NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+      </nav>
+    </header>
   );
 };
 
