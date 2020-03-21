4a37ad7c73750807dfcf0f37d718ebc3c04a545a
karenm09 <karenm929@hotmail.com>
2020-03-21 19:07:54 -0400

    add the home and video components routes

--- youtube-app/src/App.js
+++ youtube-app/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from './components/Home';
+import Video from "./components/Video";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      
+      <Switch>
+        <Route exact path={"/"}>
+          <Home />
+        </Route>
+        <Route path={"/video/:videoId"}>
+          <Video />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
