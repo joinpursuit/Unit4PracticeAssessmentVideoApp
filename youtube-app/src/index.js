f8fbe5904dfda3ce78fcda0f6df8e42f70178bbc
karenm09 <karenm929@hotmail.com>
2020-03-20 22:03:58 -0400

    move the position of the BrowserRouter

--- youtube-app/src/index.js
+++ youtube-app/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
