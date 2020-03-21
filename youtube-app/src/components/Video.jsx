6779991febcc3ce168b7a0318a790961a5d3df6f
karenm09 <karenm929@hotmail.com>
2020-03-21 19:03:24 -0400

    create the Video component

---
+++ youtube-app/src/components/Video.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from "react"
+import YouTube from 'react-youtube';
+import { useParams } from "react-router-dom";
+​
+const ShowVideo = () => {
+    let {videoId} = useParams();
+   const [name, setName] = useState();
+   const [comment, setComment] = useState();
+   const [userPosts, setUserPosts] = useState([]);
+    const nameHandle= (e) => {
+        setName(e.target.value)
+    }
+    const commentChange= (e) => {
+        setComment(e.target.value) 
+    }
+​
+    const handleSubmit = () => {
+        setUserPosts([{name, comment}, ...userPosts]);
+        setName("");
+        setComment("");
+    }
+​
+    return(
+        <div>
+            <YouTube
+                videoId={videoId}
+        // opts={opts}
+        // onReady={this._onReady}
+        />
+​
+      <form onSubmit={e => {
+          e.preventDefault();
+          handleSubmit();
+        }}>
+        <label>
+            Name:
+        <input type="text" name="name" value={name} onChange={nameHandle}/>
+        </label>
+        <label>
+            Comment:
+        <input type="text" name="comment" value={comment} onChange={commentChange}/>
+        </label>
+        <input type="submit" value="Submit"/>
+        </form>
+        {userPosts.map(post=> {
+            return(
+                <div>
+                    {post.name}
+                    {post.comment}
+                </div>
+            )
+        })}
+        </div>
+    );
+}
+export default ShowVideo
