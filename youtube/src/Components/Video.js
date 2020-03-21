1f83e8364ba5bd2c1b74d27884af095a4f3189d1
DejaLFlynn <dejaflynn@pursuit.org>
2020-03-20 23:04:09 -0400

    working on displaying video on video component or window

--- youtube/src/Components/Video.js
+++ youtube/src/Components/Video.js
@@ -1,19 +1,33 @@
 import React, {useState} from 'react'
-
+import {useParams} from "react-router-dom"
 import axios from 'axios'
-
+import YouTube from 'react-youtube' 
 const Video = ()=>{
-
+    const { id } = useParams();
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: { // https://developers.google.com/youtube/player_parameters
+          autoplay: 1
+        }
+      };
 
     return (
         <div className="Video">
     <p>
         Video
     </p>
-    
+    <YouTube
+        videoId={id}
+        opts={opts}
+        onReady={this._onReady}
+      />
             
         </div>
+        
     )
+    
 }
+
 
 export default Video
